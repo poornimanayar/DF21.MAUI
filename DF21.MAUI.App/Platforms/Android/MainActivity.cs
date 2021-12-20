9eba51a90433a7528356c6844246e297a88fc4e9
Poornima Nayar <poornima.nayar@etchuk.com>
2021-12-20 12:52:11 +0000

    Changes (I am lazy to add a nice message here)

--- DF21.MAUI.App/Platforms/Android/MainActivity.cs
+++ DF21.MAUI.App/Platforms/Android/MainActivity.cs
@@ -1,5 +1,7 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
 using Microsoft.Maui;
 
 namespace DF21.MAUI.App
@@ -7,5 +9,17 @@ namespace DF21.MAUI.App
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Microsoft.Maui.Essentials.Platform.Init(this, savedInstanceState);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            Microsoft.Maui.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 }
